c472b47429b8770fd8f8998aece3e44e178a32e4
Alberto <betim009@gmail.com>
2024-08-12 19:25:26 -0300

    script, script1

---
+++ 1-tipos_dados/script.py
@@ -0,0 +1,7 @@
+# criando uma variavel
+nome = "Alberto"  # variavel nome que recebe o valor "Alberto"
+
+print(nome)  # método print fazendo a exibição da variavel
+
+# Exibindo uma string(texto)
+print("Alberto é professor de lógica")
