27eb580111f9d09b64a53431450a404f6fc90150
unknown <betim009@gmail.com>
2025-03-28 15:21:08 -0300

    update

---
+++ pietro/main.py
@@ -0,0 +1,10 @@
+personagem1 = "Arqueiro"
+level_personagem_1 = 18
+
+personagem2 = "Guerreiro"
+level_personagem_2 = 19
+
+if level_personagem_1 > level_personagem_2:
+    print("Arqueiro Venceu")
+else:
+    print("Guerreiro Venceu")
