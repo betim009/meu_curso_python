bec9990699d934e120b7862bbca4bf1551f631d4
unknown <betim009@gmail.com>
2025-04-29 13:30:10 -0300

    update

---
+++ conteudo_aovivo/app.py
@@ -0,0 +1,19 @@
+"""
+    Processo de aprender:
+        VER/APRENDER ALGO
+
+        REPETIR
+        
+        VER/APRENDER ALGO NOVO
+        
+        REPETIR
+        
+        
+    [ ] TERMINAL - 
+    
+    [ ] TIPOS DE DADOS - 
+    
+    [ ] REPETICOES - 
+"""
+
+print("Exibe no terminal")
