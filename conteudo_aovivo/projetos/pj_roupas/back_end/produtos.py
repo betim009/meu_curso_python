d61aba659b18cf655f573b2b5e26873400896b6b
unknown <betim009@gmail.com>
2025-05-13 18:53:12 -0300

    update

---
+++ conteudo_aovivo/projetos/pj_roupas/back_end/produtos.py
@@ -0,0 +1,24 @@
+produtos = [
+    {
+        "id": 1,
+        "nome": "Camisa A",
+        "preco": 100,
+        "tamanho": "P",
+        "categoria": "Camisa",
+        "genero": "masc",
+        "marca": "Marca A",
+        "promocao": 90,
+        "lancamento": True,
+    },
+    {
+        "id": 2,
+        "nome": "Bermuda A",
+        "preco": 125,
+        "tamanho": "46",
+        "categoria": "Bermuda",
+        "genero": "masc",
+        "marca": "Marca A",
+        "promocao": False,
+        "lancamento": True,
+    },
+]
