196a0832b40aa577006670848fa13e95c69fb711
Alberto <betim009@gmail.com>
2025-07-24 14:29:33 -0300

    update

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
-def media(n1, n2):
-    nota = (n1 + n2) / 2
-    return aprovado(nota)
-
-def mediaSuperior(n1, n2, n3):
-    nota = (n1 + n2 + n3) / 3
-    return aprovado(nota)
-
-def aprovado(nota):
-    if nota > 5:
-        return "Aprovado"
-    else:
-        return "Reprovado"
-    
-print(media(5, 10))
+import requests
+credentials = {'username': 'johnd', 'password': 'm38rmF$'}
+response = requests.post('https://fakestoreapi.com/auth/login', json=credentials)
+print(response.json())
