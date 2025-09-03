9019457797fe8eb7151df64878384dd325e0207d
Alberto <betim009@gmail.com>
2025-09-03 11:36:09 -0300

    update

--- conteudo_aovivo/Flavia/projeto.py
+++ conteudo_aovivo/Flavia/projeto.py
@@ -24,6 +24,7 @@ estudantes = [
 ]
 
 while True:
+    # 
     print("Opcoes:")
     print("[1] - Listar estudantens por Idade")
     print("[2] - Listar estudantes por Curso")
@@ -31,7 +32,7 @@ while True:
     print("[4] - Listar estudantes por Genero")
     print("[5] - Encontrar um estudante pelo nome")
 
-    opcao = input("Digite a sua opcao: ")
+    opcao = input("Digite a sua opcao: ") # numero 1 - 5
 
     if opcao == "1":
         print("Listando estudantes por idade")
@@ -45,23 +46,34 @@ while True:
         print("Listando estudantes por curso")
         entrada = input("Digite o curso: ")
         # FOR
-
+        for item in estudantes:
+            if item["curso"] == entrada:
+                print(f"{item['nome']} {item['curso']}")
 
     elif opcao == "3":
         print("Listando estudantes por periodo")
         entrada = input("Digite o periodo: ")
         # FOR
+        for item in estudantes:
+            if item["periodo"] == entrada:
+                print(f"{item['nome']} {item['periodo']}")
 
     elif opcao == "4":
         print("Listando estudantes por genero")
         entrada = input("Digite o genero: ")
         # GENERO
+        for item in estudantes:
+            if item["genero"] == entrada:
+                print(f"{item['nome']} {item['genero']}")
 
     elif opcao == "5":
         print("Encontrando um usuario")
         entrada = input("Digite o nome")
         # NOME
+        for item in estudantes:
+            if item["nome"] == entrada:
+                print(f"{item['nome']}")
 
     else:
         print("Encerrando.")
-        break
+        break # Encerra
