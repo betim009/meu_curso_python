eca736c116813b1569704e127ad8d4bc2b8cc20e
Alberto <betim009@gmail.com>
2025-11-04 09:59:08 -0300

    update

---
+++ conteudo_aovivo/murillo/script2.py
@@ -0,0 +1,8 @@
+nomes = [
+    "Ana", "Bruno", "Carlos", "Daniela", "Eduardo", "Fernanda", "Gabriel",
+    "Helena", "Igor", "Juliana", "Kaio", "Larissa", "Marcos", "Natália",
+    "Otávio", "Patrícia", "Quirino", "Rafaela", "Sergio", "Tatiana"
+]
+
+for numero in range(10, 20):
+    print(nomes[numero])
